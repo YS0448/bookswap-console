807b5a5199871f0e4bcf5b29f67183d68cc65465
YS0448 <yogeshsingh0448@gmail.com>
2025-09-27 14:12:30 +0530

    datetime

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -1,5 +1,19 @@
-export const formatDate = (dateStr) => {
-  if (!dateStr) return "";
-  const date = new Date(dateStr);
-  return date.toLocaleString(); // User's local time and format
+// export const formatDate = (dateStr) => {
+//   if (!dateStr) return "";
+//   const date = new Date(dateStr);
+//   return date.toLocaleString(); // User's local time and format
+// };
+
+export const formatDate = (dbDateStr) => {
+  if (!dbDateStr) return "";
+  const date = new Date(dbDateStr + "Z"); // treat as UTC
+  return date.toLocaleString("en-GB", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: true
+  }).replace(",", "");
 };
